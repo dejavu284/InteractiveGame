405712088cf5becde9ec84a698197441b4f588d9
dejavu284 <skuratov.1@mail.ru>
2022-06-04 23:15:02 +0700

    Создал папку и репозиторий под свою первую интерактивную консольную игру

---
+++ InteractiveGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InteractiveGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Будущая игра находится на этапе мозгования)))");
+        }
+    }
+}
