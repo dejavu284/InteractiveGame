1de5e615abc5d40cf944bca6e28b33d9b43ad74b
dejavu284 <skuratov.1@mail.ru>
2022-06-05 21:11:05 +0700

    Добавил реализацию инвентара v1.0; Добавил реализацию объектов v1.0

--- InteractiveGame/Program.cs
+++ InteractiveGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 // Суть игры будет заключаться во взаимодействии со внешним виртуальным ограниченным миром
 // Нужно осуществить взаимодействие игрока с инвентарём и со внешним миром
 // Игра будет пошаговой, то есть игрок может делать какие-то действия и каждое действие будет считаться ходом,
@@ -36,7 +37,107 @@ namespace InteractiveGame
     {
         static void Main(string[] args)
         {
+            int StartCapaciti = 4;
+            InventoriMy Inventori = new InventoriMy(StartCapaciti);
+            Inventori.Add(1, new Knife());
+            Inventori.Add(4, new Pistol());
+            Inventori.ShowInventoryContent();
+            Console.WriteLine();
+            Inventori.InventoryExtension(1);
+            Inventori.ShowInventoryContent();
+            Console.WriteLine();
+            Inventori.InventoryExtension(1);
+            Inventori.ShowInventoryContent();
+            Console.WriteLine();
             Console.WriteLine("Будущая игра находится на этапе мозгования)))");
+        }
+    }
+    abstract class Item
+    {
+        public abstract bool CanDamage { get; }
+        public abstract bool CanHelp { get; }
+    }
+    abstract class Weapon: Item
+    {
+        public override bool CanDamage { get { return true; } }
+        public override bool CanHelp => true;
+        public abstract int Damage { get; }
+        public abstract void Fire();
+        public void Showinfo()
+        {
+            Console.WriteLine("{0} урон {1}", GetType().Name, Damage);
+        }
+    }
+    class Pistol : Weapon
+    {
+        public override int Damage => 10;
+
+        public override void Fire()
+        {
+            Console.WriteLine("Стреляю из объекта - {0}", GetType().Name);   
+        }
+    }
+    class Knife : Weapon
+    {
+        public override int Damage => 7;
+
+        public override void Fire()
+        {
+            Console.WriteLine("Ударил объектом - {0}", GetType().Name);
+        }
+    }
+    class Book : Item
+    {
+        public override bool CanDamage => false;
+        public override bool CanHelp => false;
+    }
+    class InventoryWorld
+    {
+        public List<Item> WorldInventory = new List<Item>() { new Book(), new Knife(), new Pistol()};
+        public void Add(int position, Item item)
+        {
+            position = WorldInventory.Count;
+            
+        }
+    }
+    class InventoriMy
+    {
+        public InventoriMy(int capacity)
+        {
+            while (capacity > 0)
+            {
+                MyInventori.Add(null);
+                capacity--;
+            }
+            Console.WriteLine("Создан пустой инвентарь на {0} слота", MyInventori.Count);
+        }
+        public List<Item> MyInventori = new List<Item>();
+
+        public void Add(int position, Item item)
+        {
+            position--;
+            MyInventori[position] = item;
+        }
+        public void ShowInventoryContent()
+        {
+            string Print = "      ";
+            string Sep = "";
+            foreach (var item in MyInventori)
+            {
+                if (item is null) Print += "|  Пусто  |      ";
+                else Print += ("|  " + item.GetType().Name + "  |      ");
+            }
+            Sep = Sep.PadLeft(Print.Length, '=');
+
+            Console.WriteLine(Sep + "\n" + Print + "\n" + Sep);
+        }
+        public void InventoryExtension(int quantity)
+        {
+            while (quantity > 0)
+            {
+                MyInventori.Add(null);
+                quantity--;
+            }
         }
     }
 }
